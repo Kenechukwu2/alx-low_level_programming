58000acda9ab78b2cfdaa0e74aaabfd0b570b4e2
Kenechukwu2 <nwobukenechukwu@gmail.com>
2022-09-16 21:36:40 -0700

    sidegig

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	print("Infinie loop incoming :(/n");
+	
+	i = 0;
+	
+	/*while(i<10)*/
+	/*{*/
+	/*putachar(i);*/
+	/*}*/
+
+	print("infinite loop avoided!\\o/\n"0);
+
+	return (0);
